c9b66a8ea0a75c8e35e35d64a156fbf7309889ff
Rizwan Hussain <rizwan.ah07@hotmail.com>
2024-05-26 18:12:53 +0000

    fixed hamburger menu bug

--- Milestone-2/public/js/script.js
+++ Milestone-2/public/js/script.js
@@ -1,12 +1,19 @@
 document.addEventListener('DOMContentLoaded', function () {
     var hamburger = document.getElementById('hamburger');
     var navMenu = document.getElementById('nav-menu');
- 
-    hamburger.addEventListener('click', function () {
-        if (navMenu.style.width == '350px') {
-            navMenu.style.width = '0';
-        } else {
-            navMenu.style.width = '350px';
-        }
-    });
+
+    if (hamburger && navMenu) {
+        console.log('Hamburger and navMenu found');
+        hamburger.addEventListener('click', function () {
+            if (navMenu.style.width == '350px') {
+                navMenu.style.width = '0';
+                console.log('Nav menu closed');
+            } else {
+                navMenu.style.width = '350px';
+                console.log('Nav menu opened');
+            }
+        });
+    } else {
+        console.error('Hamburger or navMenu not found');
+    }
 });
